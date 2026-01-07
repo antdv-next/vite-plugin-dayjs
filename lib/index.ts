64f5be4567d83903cd8c15ffb4be0e3fbadc6c03
aibayanyu20 <aibayanyu@qq.com>
2026-01-07 12:22:26 +0800

    chore: change

--- lib/index.ts
+++ lib/index.ts
@@ -11,33 +11,22 @@ export function vitePluginDayjs(): Plugin {
 
       // 2) 根入口：dayjs -> dayjs/esm
       if (source === 'dayjs') {
-        const resolved = await this.resolve('dayjs/esm', importer, { skipSelf: true, ...options })
-        return resolved
+        return await this.resolve('dayjs/esm', importer, { skipSelf: true, ...options })
       }
 
       // 3) 插件入口的多种写法
-      //    - dayjs/plugin/xxx/index.js     -> dayjs/esm/plugin/xxx/index.js
       //    - dayjs/plugin/xxx.js           -> dayjs/esm/plugin/xxx/index.js
       //    - dayjs/plugin/xxx              -> dayjs/esm/plugin/xxx
-      const pluginIndex = source.match(/^dayjs\/plugin\/([^/]+)\/index\.js$/)
-      if (pluginIndex) {
-        const target = `dayjs/esm/plugin/${pluginIndex[1]}/index.js`
-        const resolved = await this.resolve(target, importer, { skipSelf: true, ...options })
-        return resolved
-      }
-
       const pluginWithJs = source.match(/^dayjs\/plugin\/([^/]+)\.js$/)
       if (pluginWithJs) {
         const target = `dayjs/esm/plugin/${pluginWithJs[1]}/index.js`
-        const resolved = await this.resolve(target, importer, { skipSelf: true, ...options })
-        return resolved
+        return await this.resolve(target, importer, { skipSelf: true, ...options })
       }
 
       const pluginBare = source.match(/^dayjs\/plugin\/([^/]+)$/)
       if (pluginBare) {
         const target = `dayjs/esm/plugin/${pluginBare[1]}`
-        const resolved = await this.resolve(target, importer, { skipSelf: true, ...options })
-        return resolved
+        return await this.resolve(target, importer, { skipSelf: true, ...options })
       }
 
       return null
